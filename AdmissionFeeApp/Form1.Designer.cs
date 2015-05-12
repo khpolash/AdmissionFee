13d75769a4ecd2470e3f777d9e9f97fcaf0f5db4
khpolash <khasan994@gmail.com>
2015-05-12 11:15:33 +0600

    Creat design and operation

--- AdmissionFeeApp/Form1.Designer.cs
+++ AdmissionFeeApp/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.calculateBbutton = new System.Windows.Forms.Button();
+            this.discountCheckBox = new System.Windows.Forms.CheckBox();
+            this.selectClassComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Class";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.calculateBbutton);
+            this.groupBox1.Controls.Add(this.discountCheckBox);
+            this.groupBox1.Controls.Add(this.selectClassComboBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(480, 231);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculator for yearly cost of a student";
+            // 
+            // calculateBbutton
+            // 
+            this.calculateBbutton.Location = new System.Drawing.Point(154, 158);
+            this.calculateBbutton.Name = "calculateBbutton";
+            this.calculateBbutton.Size = new System.Drawing.Size(192, 29);
+            this.calculateBbutton.TabIndex = 3;
+            this.calculateBbutton.Text = "Calculate total amount for one year";
+            this.calculateBbutton.UseVisualStyleBackColor = true;
+            this.calculateBbutton.Click += new System.EventHandler(this.calculateBbutton_Click);
+            // 
+            // discountCheckBox
+            // 
+            this.discountCheckBox.AutoSize = true;
+            this.discountCheckBox.Location = new System.Drawing.Point(154, 99);
+            this.discountCheckBox.Name = "discountCheckBox";
+            this.discountCheckBox.Size = new System.Drawing.Size(192, 17);
+            this.discountCheckBox.TabIndex = 2;
+            this.discountCheckBox.Text = "Get 5% discount for early admission";
+            this.discountCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // selectClassComboBox
+            // 
+            this.selectClassComboBox.FormattingEnabled = true;
+            this.selectClassComboBox.Items.AddRange(new object[] {
+            "Select Class"});
+            this.selectClassComboBox.Location = new System.Drawing.Point(154, 51);
+            this.selectClassComboBox.Name = "selectClassComboBox";
+            this.selectClassComboBox.Size = new System.Drawing.Size(192, 21);
+            this.selectClassComboBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(504, 277);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Admission Fee UI";
+            this.Load += new System.EventHandler(this.onLoad);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button calculateBbutton;
+        private System.Windows.Forms.CheckBox discountCheckBox;
+        private System.Windows.Forms.ComboBox selectClassComboBox;
     }
 }
 
--- AdmissionFeeApp/Form1.cs
+++ AdmissionFeeApp/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AdmissionFeeApp
@@ -15,6 +9,63 @@ namespace AdmissionFeeApp
         public Form1()
         {
             InitializeComponent();
+        }
+        List<int>  admissionFeeList = new List<int>();
+        List<int>  monthlyList = new List<int>();
+        private void onLoad(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                selectClassComboBox.Items.Add(@"Class " + i);
+            }
+            admissionFeeList.Add(10000);
+            admissionFeeList.Add(10000);
+            admissionFeeList.Add(10000);
+            admissionFeeList.Add(10000);
+            admissionFeeList.Add(10000);
+            admissionFeeList.Add(12000);
+            admissionFeeList.Add(12000);
+            admissionFeeList.Add(12000);
+            admissionFeeList.Add(15000);
+            admissionFeeList.Add(15000);
+            admissionFeeList.Add(15000);
+            admissionFeeList.Add(15000);
+
+            monthlyList.Add(500);
+            monthlyList.Add(500);
+            monthlyList.Add(1000);
+            monthlyList.Add(1000);
+            monthlyList.Add(1000);
+            monthlyList.Add(1500);
+            monthlyList.Add(1500);
+            monthlyList.Add(1500);
+            monthlyList.Add(2000);
+            monthlyList.Add(2000);
+            monthlyList.Add(2700);
+            monthlyList.Add(2700);
+            
+        }
+
+        private void calculateBbutton_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <=13; i++)
+            {
+                if (@"Class "+i == selectClassComboBox.Text)
+                {
+                    double totalAmount = admissionFeeList[i-1] + (monthlyList[i-1]*12);
+                    if (discountCheckBox.Checked)
+                    {
+                        MessageBox.Show(Convert.ToString(totalAmount-totalAmount*.05));
+                    }
+                    else
+                    {
+                        MessageBox.Show(Convert.ToString(totalAmount));
+                    }
+                    
+                }
+                
+            }
+            
         }
     }
 }
